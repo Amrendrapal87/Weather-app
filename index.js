358b61cbd79be1af5dcf4da2a98bcaa24af4f37a
Amrendra pal <amrendrapal7777@gmail.com>
2025-12-27 17:25:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+var inputval=document.querySelector("#cityinput");
+var btn=document.querySelector("#add");
+var cityname=document.querySelector("#cityoutput");
+var temp=document.querySelector("#temp");
+var desc=document.querySelector("#description");
+var humid=document.querySelector("#humid");
+var windsp=document.querySelector("#wind");
+var rain=document.querySelector("#rain");
+
+apik = '6c6f0a22ad1aff16a9025e9eb4b17766';
+
+btn.addEventListener("click",function(){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputval.value}&appid=${apik}&units=metric`)
+    .then(response => response.json())
+    .then(data => {
+        // console.log(data);
+        var nameval=data['name'];
+        var tempval=data['main']['temp'];
+        var descval=data['weather'][0]['description'];
+        var humidval=data['main']['humidity'];
+        var rainval=data['rain'];
+
+        var windval=data['wind']['speed'];
+        cityname.innerHTML=nameval;
+        temp.innerHTML="Temperature: " + tempval + " °C";
+        desc.innerHTML="Description: " + descval;
+        windsp.innerHTML="Wind Speed: " + windval + " m/s";
+        humid.innerHTML="Humidity: " + humidval + " %";
+        rain.innerHTML="rain: " +rainval + "0 %";
+    })
+    .catch(err => alert("Wrong city name!"));
+
+});
