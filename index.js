f051a8ed5849a9ae3ca3f4bef4faaad31e3d6e01
Amrendra pal <amrendrapal7777@gmail.com>
2025-12-27 18:36:52 +0530

    Enhance weather app with more details
    Added additional weather information display including geography, sunrise, sunset, and weather condition.

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ var humid=document.querySelector("#humid");
 var windsp=document.querySelector("#wind");
 var rain=document.querySelector("#rain");
 var pressure=document.querySelector("#pressure");
-precipitation=document.querySelector("#precipitation");
+var precipitation=document.querySelector("#precipitation");
+var geography=document.querySelector("#geography");
+var sunrise=document.querySelector("#sunrise");
+var sunset=document.querySelector("#sunset");
+var thunder=document.querySelector("#thunder");
+var earthval=document.querySelector("#earthval");
+var northval=document.querySelector("#northval");
+var geographyval=document.querySelector("#geographyval");
+
 
 apik = '6c6f0a22ad1aff16a9025e9eb4b17766';
 
@@ -23,6 +31,13 @@ btn.addEventListener("click",function(){
         var rainval=data['rain'];
          var pressureval=data['main']['pressure'];
          var precipitationval=data['clouds']['all'];
+         var geographyval=data['sys']['country'];
+        var sunriseval=data['sys']['sunrise'];
+        var sunsetval=data['sys']['sunset'];
+        var thunderval=data['weather'][0]['main'];
+        var earthval=data['coord']['lon'];
+        var northval=data['coord']['lat'];
+        var geographyval= northval + " N , " + earthval + " E";
 
         var windval=data['wind']['speed'];
         cityname.innerHTML=nameval;
@@ -32,10 +47,18 @@ btn.addEventListener("click",function(){
         humid.innerHTML="Humidity: " + humidval + " %";
         rain.innerHTML="rain: " +rainval + "0 %";
         pressure.innerHTML="Pressure: " + pressureval + " hPa";
-         precipitation.innerHTML="Precipitation: " + precipitationval + " %";
+        precipitation.innerHTML="Precipitation: " + precipitationval + " %";
+        geography.innerHTML="Geography: " + geographyval;
+        sunrise.innerHTML="Sunrise: " + new Date(sunriseval * 1000).toLocaleTimeString();
+        sunset.innerHTML="Sunset: " + new Date(sunsetval * 1000).toLocaleTimeString();
+        thunder.innerHTML="Weather Condition: " + thunderval;
+        earthval.innerHTML="Longitude: " + earthval + " E";
+        northval.innerHTML="Latitude: " + northval + " N";
+        geographyval.innerHTML="Coordinates: " + geographyval;
     })
     .catch(err => alert("Wrong city name!"));
 
 
 });
+
 
